f85e248939c1755f3aa827432b62259f77ede45f
DrewGregory <djgregny@gmail.com>
2017-01-08 14:47:49 -0500

    Initial Commit

---
+++ FLHS_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FLHS_App
+//
+//  Created by Drew Gregory on 1/8/17.
+//  Copyright © 2017 FLHS Computer Science Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
